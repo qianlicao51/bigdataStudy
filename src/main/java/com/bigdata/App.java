abaecf4414c1cbedfcbe13964623f6ddb4ed3660
DESKTOP-C5GAJG4\Administrator <qianlicaodemo@163.com>
2019-11-25 19:20:35 +0800

    logback

--- src/main/java/com/bigdata/App.java
+++ src/main/java/com/bigdata/App.java
@@ -1,14 +1,24 @@
 package com.bigdata;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App {
-	public static void main(String[] args) {
+	static Logger logger = LoggerFactory.getLogger(App2.class);
+
+	public static void main(String[] args) throws URISyntaxException {
 
 		log.info("主程序执行 |{}", new DateTime().toString("yyyy-MM-dd HH:mm:ss:SSS"));
-
+		logger.info("主程序执行logger |{}", new DateTime().toString("yyyy-MM-dd HH:mm:ss:SSS"));
+		URI uri = ClassLoader.getSystemResource("application.properties").toURI();
+		System.out.println(uri.toString());
 	}
 
 }
